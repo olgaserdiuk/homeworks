d6c9db77277abf052653e499de7548a27cc54a3f
Olga Serdiuk <enigma_so@mail.ru>
2017-05-14 21:55:04 +0300

    2nd HW was completed

--- _1-2_js_HW/script.js
+++ _1-2_js_HW/script.js
@@ -1,4 +1,4 @@
-/*возведение числа в степень*/
+/*1. Возведение числа в степень*/
 
 var a = prompt('Укажите число для возведения в степень', 2);
 var n = prompt('Укажите степень в которую возвести число', 3);
@@ -11,6 +11,7 @@ function pow(a, n) {
 	}
 
 	return b;
+
 }
 
 // if (a == '') {
@@ -30,4 +31,42 @@ alert('Результат ' + pow(a, n));
 console.log(pow(a,n));
 
 
+
+/*2. Проверить имя пользователя*/
+
+var arr=[]; 
+    for(var i = 0; i<5; i++) arr.push(prompt('Enter your name'));
+
+console.log(arr);
+
+var userName = prompt('Enter your Username');
+
+if (userName == arr[0]) {
+	
+	alert( 'Welcome ' + arr[0]);
+
+	} else if (userName == arr[1]) {
+	
+	alert( 'Welcome ' + arr[1]);
+
+	} else if (userName == arr[2]) {
+	
+	alert( 'Welcome ' + arr[2]);
+
+	} else if (userName == arr[3]) {
+	
+	alert( 'Welcome ' + arr[3]);
+
+	} else if (userName == arr[4]) {
+	
+	alert( 'Welcome ' + arr[4]);
+
+	} else if (userName == '') {
+	
+	alert( 'Enter your Username' );
+
+	} else {
+
+  alert( 'Invalid Username' );
+}
 
