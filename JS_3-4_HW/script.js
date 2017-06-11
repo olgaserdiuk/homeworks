bdab5915c31d80fa92b592eeb8acca9f7862b9c7
Olga Serdiuk <enigma_so@mail.ru>
2017-06-11 20:04:05 +0300

    homework was ended

--- JS_3-4_HW/script.js
+++ JS_3-4_HW/script.js
@@ -1,83 +1,70 @@
-var title = document.createElement('h1');
-title.innerHTML = 'Тест по программированию';
-var wrapper = document.querySelector('wrapper');
-wrapper.appendChild(title);
 
-
-var questions = document.createElement('h3');
-questions.innerHTML = 'Вопрос №1';
-var wrapper = document.querySelector('wrapper');
-wrapper.appendChild(questions);
-
-var div = document.createElement('div');
-wrapper.appendChild(div);
-
-var checkbox = document.createElement('input');
-checkbox.type = "checkbox";
-checkbox.id = "id";
-
-var label = document.createElement('label')
-label.htmlFor = "id";
-label.appendChild(document.createTextNode('Вариант ответа №1'));
-
-div.appendChild(checkbox);
-div.appendChild(label);
-
-
-
-// создать копию узла
-var checkbox2 = checkbox.cloneNode(true);
-var label2 = label.cloneNode(true);
-  // копию можно подправить
-label2.innerHTML = 'Вариант ответа №2';
-  // вставим её после текущего сообщения
-div.appendChild(checkbox2, checkbox);
-div.appendChild(label2, label);
-
-
-var checkbox3 = checkbox.cloneNode(true);
-var label3 = label.cloneNode(true);
-
-label3.innerHTML = 'Вариант ответа №3';
-
-div.appendChild(checkbox3, checkbox);
-div.appendChild(label3, label);
-
-
-var questions2 = questions.cloneNode(true);
-questions2.innerHTML = 'Вопрос №2';
-wrapper.appendChild(questions2, questions);
-
-
-var div2 = div.cloneNode(true);
-wrapper.appendChild(div2, div);
-
-
-var questions3 = questions.cloneNode(true);
-questions3.innerHTML = 'Вопрос №3';
-wrapper.appendChild(questions3, questions);
-
-var div3 = div.cloneNode(true);
-wrapper.appendChild(div3, div);
-
-
-
-/*var test = {
+var test = {
   data: {
     title: 'Тест по какой-то теме',
     questions: [
       {
         title: 'Вопрос #1',
-        answers: ['Вариант овтета 1', 'Вариант овтета 2', 'Вариант овтета 3']
+        answers: ['Вариант ответа 1', 'Вариант ответа 2', 'Вариант ответа 3']
       },
       {
         title: 'Вопрос #2',
-        answers: ['Вариант овтета 1', 'Вариант овтета 2', 'Вариант овтета 3', 'Вариант овтета 4']
+        answers: ['Вариант ответа 1', 'Вариант ответа 2', 'Вариант ответа 3', 'Вариант ответа 4']
       },
       {
         title: 'Вопрос #3',
-        answers: ['Вариант овтета 1', 'Вариант овтета 2']
+        answers: ['Вариант ответа 1', 'Вариант ответа 2']
       }
     ]
-  }
-};*/
+  },
+  
+  myWrapper: document.querySelector('wrapper'),
+
+  genHtml: function() {
+    var title = document.createElement('h1');
+    title.appendChild(document.createTextNode(this.data.title));
+    this.myWrapper.appendChild(title);
+    var questions = this.genQuestions();
+    this.myWrapper.appendChild(questions);
+    this.myWrapper.appendChild(this.addButton());
+  },
+
+  genQuestions: function(){
+      var div = document.createElement('div');
+      this.data.questions.forEach(function(oneQuestion, i) {
+        var questionTitle = document.createElement('h3');
+        questionTitle.appendChild(document.createTextNode(oneQuestion.title));
+        div.appendChild(questionTitle);
+
+        var divCheck = document.createElement('div');
+
+        oneQuestion.answers.forEach(function(oneAnswer, n) {
+          var checkbox = document.createElement('input');
+          checkbox.type = "checkbox";
+          checkbox.id = "check" + n;
+
+          var label = document.createElement('label')
+          label.htmlFor = checkbox.id;
+          label.appendChild(document.createTextNode(oneAnswer));
+
+          divCheck.appendChild(checkbox);
+          divCheck.appendChild(label);
+        });
+
+        div.appendChild(divCheck);
+      });
+
+      return div;     
+  },
+
+  addButton: function() {
+    var button = document.createElement('button');
+    button.type = "submit";
+    button.appendChild(document.createTextNode( 'Проверить мои результаты'));
+
+    return button;
+  },
+
+};
+
+test.genHtml();
