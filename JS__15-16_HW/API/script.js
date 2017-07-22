71f4905b68df7548d340f31b89eb7565c213e4ea
Olga Serdiuk <enigma_so@mail.ru>
2017-07-22 21:56:21 +0300

    change doc.ready and placeholder

--- JS__15-16_HW/API/script.js
+++ JS__15-16_HW/API/script.js
@@ -1,7 +1,4 @@
-$(document).ready(function(){
-
-
-
+$(function(){
 
 $('.myButton').click( function() {
 	event.preventDefault();
--- JS__15-16_HW/var test.js
+++
@@ -1,5 +0,0 @@
-var test = {
-	question: 'Вопрос',
-	answers: ['1', '2', '3'],
-	rightAns: 'правильный ответ',
-}
